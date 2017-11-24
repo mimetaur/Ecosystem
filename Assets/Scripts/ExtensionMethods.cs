d70a0693b5abc5d49182f3e69178041aeb3d901f
Nathan Koch <nathanrkoch@gmail.com>
2017-11-24 11:23:42 -0500

    steering toward target

---
+++ Assets/Scripts/ExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ExtensionMethods
+{
+    public static Vector2 AsVector2(this Vector3 v) {
+        return new Vector2(v.x, v.y);
+    }
+
+    public static float Map(this float s, float a1, float a2, float b1, float b2)
+    {
+        return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
+    }
+
+}
--- Assets/Scripts/Steer.cs
+++ Assets/Scripts/Steer.cs
@@ -2,28 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Steer : MonoBehaviour {
+public class Steer : MonoBehaviour
+{
 
-    Rigidbody2D rb2d;
-    Vector2 acceleration;
-    float rotationAcceleration;
+    public Vector2 target;
+    private Rigidbody2D rb2d;
+    private float maxSpeed;
 
-	// Use this for initialization
-	void Start () {
+    private void Start()
+    {
         rb2d = GetComponent<Rigidbody2D>();
+        maxSpeed = Random.Range(1.5f, 4.0f);
+        target = new Vector2(-20, transform.position.y);
 
-        float lowerLimit = -0.25f;
-        float upperLimit = -0.75f;
-        float xAccel = Random.Range(lowerLimit, upperLimit);
-        acceleration = new Vector2(xAccel, 0.0f);
-        rotationAcceleration = GameUtils.Map(xAccel, lowerLimit, upperLimit, 0.05f, 0.15f);
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-        rb2d.AddForce(acceleration);
-        rb2d.AddTorque(rotationAcceleration);
-	}
+        //float lowerLimit = -0.25f;
+        //float upperLimit = -0.75f;
+        //float xAccel = Random.Range(lowerLimit, upperLimit);
+        //acceleration = new Vector2(xAccel, 0.0f);
+        //rotationAcceleration = GameUtils.Map(xAccel, lowerLimit, upperLimit, 0.05f, 0.15f);
+    }
+
+    private Vector2 Seek()
+    {
+        Vector2 position = transform.position.AsVector2();
+        Vector2 desired = target - position;
+        desired.Normalize();
+        desired *= maxSpeed;
+
+        Vector2 steer = desired - rb2d.velocity;
+        steer.Normalize();
+        steer *= maxSpeed;
+        return steer;
+    }
+
+    private void FixedUpdate()
+    {
+        var steer = Seek();
+        rb2d.AddForce(steer);
+        rb2d.velocity = Vector2.ClampMagnitude(rb2d.velocity, maxSpeed);
+
+        rb2d.angularVelocity = 125.0f;
+
+        //rb2d.AddForce(acceleration);
+        //rb2d.AddTorque(rotationAcceleration);
+    }
 }
 
 
