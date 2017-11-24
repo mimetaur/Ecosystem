5148e1cb817eea70a1826c6c9c9a9f87a40fab49
Nathan Koch <nathanrkoch@gmail.com>
2017-11-24 08:43:31 -0500

    cleanup. confirming presence of pathfinding namespace

--- Assets/Scripts/AStarAI.cs
+++ Assets/Scripts/AStarAI.cs
@@ -1,7 +1,5 @@
 ﻿using UnityEngine;
 using System.Collections;
-//Note this line, if it is left out, the script won't know that the class 'Path' exists and it will throw compiler errors
-//This line should always be present at the top of scripts which use pathfinding
 using Pathfinding;
 
 public class AStarAI : MonoBehaviour
