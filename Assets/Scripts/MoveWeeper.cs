ec8327ccf3a49b3b4218a73532df84518d0118a6
Nathan Koch <nathanrkoch@gmail.com>
2017-11-25 14:12:16 -0500

    weeper alternates between seeking and wandering

--- Assets/Scripts/MoveWeeper.cs
+++ Assets/Scripts/MoveWeeper.cs
@@ -7,31 +7,41 @@ public class MoveWeeper : MonoBehaviour
 {
 
     public float radius;
-    public Vector2 target;
 
     IAstarAI ai;
     AIStateMachine machine;
+    ScanForCoins scan;
 
 
     private void Start()
     {
         ai = GetComponent<IAstarAI>();
         machine = GetComponent<AIStateMachine>();
+        scan = GetComponent<ScanForCoins>();
 
-        ai.destination = PickRandomPoint();
+        ai.destination = GetRandomPoint();
     }
 
     private void Update()
     {
         if (!ai.pathPending && (ai.reachedEndOfPath || !ai.hasPath))
         {
-            ai.destination = PickRandomPoint();
+            print("End of path");
+            if (machine.currentState == AIStateMachine.State.Wander) {
+                ai.destination = GetRandomPoint();     
+            } else if (machine.currentState == AIStateMachine.State.Seek) {
+                ai.destination = Seek();
+            }
             ai.SearchPath();
         }
     }
 
+    private Vector2 Seek()
+    {
+        return scan.closestCoinLocation;
+    }
 
-    private Vector2 PickRandomPoint()
+    private Vector2 GetRandomPoint()
     {
         var point = Random.insideUnitCircle * radius;
         point += transform.position.AsVector2();
--- Assets/Scripts/ScanForCoins.cs
+++ Assets/Scripts/ScanForCoins.cs
@@ -8,7 +8,7 @@ public class ScanForCoins : MonoBehaviour
 
     AIStateMachine machine;
 
-    public Vector2 coinLocation;
+    public Vector2 closestCoinLocation;
 
     private void Start()
     {
@@ -16,19 +16,43 @@ public class ScanForCoins : MonoBehaviour
     }
 
     void Update()
-    {
-        if (machine.currentState != AIStateMachine.State.Wander) return;
-            
+    {            
         var coins = GameObject.FindGameObjectsWithTag("Pickup");
-        for (int i = 0; i < coins.Length; i++)
-        {
-            var coin = coins[i];
-            var coinPos = coin.transform.position.AsVector2();
+        var closestCoin = GetClosest(coins);
+        if (closestCoin != null) {
+            var coinPos = closestCoin.transform.position.AsVector2();
             var myPos = transform.position.AsVector2();
 
-            if (Vector2.Distance(coinPos, myPos) < threshold) {
+            if (Vector2.Distance(coinPos, myPos) < threshold)
+            {
                 machine.currentState = AIStateMachine.State.Seek;
+                closestCoinLocation = coinPos;
+            } else {
+                machine.currentState = AIStateMachine.State.Wander;
+            }
+        } else {
+            machine.currentState = AIStateMachine.State.Wander;
+        }
+    }
+
+
+    // Example algorithm sourced from:
+    // https://docs.unity3d.com/ScriptReference/GameObject.FindGameObjectsWithTag.html
+    private GameObject GetClosest(GameObject[] things)
+    {
+        GameObject closestThing = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject thing in things)
+        {
+            Vector3 diff = thing.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closestThing = thing;
+                distance = curDistance;
             }
         }
+        return closestThing;
     }
 }
