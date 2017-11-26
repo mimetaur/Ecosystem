380c24b7273ef76124b4c3581e10adbec7d7f4fa
Nathan Koch <nathanrkoch@gmail.com>
2017-11-26 10:48:01 -0500

    Camera pans around the game world

---
+++ Assets/Scripts/Pan.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pan : MonoBehaviour
+{
+    [Range(5.0f, 120.0f)]
+    public float panRate = 10.0f;
+
+    public float smoothTime = 0.3f;
+
+    private GameWorld gameWorld;
+
+    private Vector3 targetPosition;
+
+    private float zOffset;
+
+    private Vector3 velocity = Vector3.zero;
+
+    void Start()
+    {
+        gameWorld = GameObject.Find("GameManager").GetComponent<GameWorld>();
+        zOffset = transform.position.z;
+        InvokeRepeating("SetRandomTarget", 0.0f, panRate);
+    }
+
+    private void SetRandomTarget()
+    {
+        targetPosition = gameWorld.GetRandomLocation();
+
+    }
+
+    void LateUpdate()
+    {
+        var newPosition = new Vector3(targetPosition.x, targetPosition.y, zOffset);
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+    }
+}
