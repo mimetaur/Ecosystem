3fbc6696470606cf8fcd5ad2bd615ba8d00c550f
Nathan Koch <nathanrkoch@gmail.com>
2017-11-26 09:56:15 -0500

    Prefab tweaks

--- Assets/Scripts/EatPickup.cs
+++ Assets/Scripts/EatPickup.cs
@@ -21,7 +21,7 @@ public class EatPickup : MonoBehaviour
         if (other.tag == tagName)
         {
             machine.currentState = AIStateMachine.State.Wander;
-            string debug = string.Format("Ate {0} at {1}", tagName, other.transform.position.AsVector2());
+            string debug = string.Format("{0} ate {1} at {2}", name, tagName, other.transform.position.AsVector2());
             print(debug);
             Destroy(other.gameObject);
         }
