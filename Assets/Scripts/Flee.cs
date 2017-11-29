a8a14bfc3fabd5f79b73b4645a9d10e794926946
Nathan Koch <nathanrkoch@gmail.com>
2017-11-29 12:09:29 -0500

    hungry icon above hungry hands

--- Assets/Scripts/Flee.cs
+++ Assets/Scripts/Flee.cs
@@ -20,7 +20,6 @@ public class Flee : MonoBehaviour
 
         exclamationMark = (GameObject)Instantiate(Resources.Load("Exclamation Mark"));
         exclamationMark.transform.parent = this.transform;
-        //exclamationMark.transform.position = new Vector3(transform.position.x, transform.position.y - 2, transform.position.z);
         exclamationMark.SetActive(false);
     }
 
@@ -30,36 +29,21 @@ public class Flee : MonoBehaviour
     {
         if (machine.currentState != AIStateMachine.State.Flee || predator == null)
         {
-            //spriteRenderer.color = Color.white;
             exclamationMark.SetActive(false);
             return;
         }
         exclamationMark.SetActive(true);
         exclamationMark.transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
 
-        //spriteRenderer.color = Color.red;
-
         ai.destination = FleePredator();
         ai.SearchPath();
-
-        //if (!ai.pathPending && (ai.reachedEndOfPath || !ai.hasPath))
-        //{
-        //    ai.destination = FleePredator();
-        //    ai.SearchPath();
-        //}
 
     }
 
     private Vector2 FleePredator()
     {
-        //var pos = predator.transform.position.AsVector2();
-        //var dir = pos.normalized;
-        //var oppDir = pos *= -1;
         float desiredDistance = 10.0f;
         var point = predator.transform.position + (transform.position - predator.transform.position).normalized * desiredDistance;
-
-        //var point = oppDir * 12.0f;
-        //point += transform.position.AsVector2();
 
         return point;
     }
---
+++ Assets/Scripts/Predator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Predator : MonoBehaviour {
+
+    private AIStateMachine machine;
+
+    private GameObject hungryIcon;
+
+
+	void Start () {
+        machine = GetComponent<AIStateMachine>();
+
+        hungryIcon = (GameObject)Instantiate(Resources.Load("Hungry"));
+        hungryIcon.transform.parent = this.transform;
+        hungryIcon.SetActive(false);
+	}
+	
+	void Update () {
+        if (machine.currentState != AIStateMachine.State.Seek)
+        {
+            hungryIcon.SetActive(false);
+            return;
+        } else {
+            hungryIcon.SetActive(true);
+            hungryIcon.transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+        }
+	}
+}
