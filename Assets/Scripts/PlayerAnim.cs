c09ab107eab5a6bbb2ed6e92597baaa4e257c8ef
Nathan Koch <nathanrkoch@gmail.com>
2017-12-07 14:09:13 -0500

    flips x axis based on target location

--- Assets/Scripts/PlayerAnim.cs
+++ Assets/Scripts/PlayerAnim.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class PlayerAnim : MonoBehaviour
 {
 
     QuestAIStateMachine machine;
-
     Animator anim;
+    SpriteRenderer sr;
+
+    IAstarAI ai;
     // Use this for initialization
     void Start()
     {
         machine = GetComponent<QuestAIStateMachine>();
         anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+        ai = GetComponent<IAstarAI>();
     }
 
     // Update is called once per frame
@@ -25,6 +30,14 @@ public class PlayerAnim : MonoBehaviour
         else
         {
             anim.enabled = true;
+            if (transform.position.x > ai.destination.x)
+            {
+                sr.flipX = true;
+            }
+            else
+            {
+                sr.flipX = false;
+            }
         }
     }
 }
